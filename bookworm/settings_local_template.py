164bb037fbcf7d519abc16d61a709389209f75a8
muavr <muravevanton@gmail.com>
2018-12-08 13:01:29 +0300

    gh-1 Initialize basic django application
    Moves db credentials and django secret in setting_local.py file.
    File setting_local_template.py was created.
    New rules were added in .gitignore file:
        - default sqlite database
        - setting_local.py

---
+++ bookworm/settings_local_template.py
@@ -0,0 +1,22 @@
+import os
+
+# Build paths inside the project like this: os.path.join(BASE_DIR, ...)
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = 'secret'
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = False
+
+ALLOWED_HOSTS = ['127.0.0.1']
+
+# Database
+# https://docs.djangoproject.com/en/2.1/ref/settings/#databases
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'bookwarm.db.sqlite3'),
+    }
+}
